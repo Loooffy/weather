2715d42838a553725a0669d9290704cb0964cd9f
Loooffy <Loooffy@gmail.com>
2020-02-14 15:59:23 +0800

    add google api

--- quickstart.py
+++ quickstart.py
@@ -1,35 +1,9 @@
-import requests
-import json
-import csv
 from __future__ import print_function
 import pickle
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
-
-auth="CWB-C216538E-45E8-4FB5-BFAD-09ACD2B62F94"
-base='https://opendata.cwb.gov.tw/fileapi/v1/opendataapi/'
-dataid="O-A0001-001"
-stationid='466990'
-elem="TEMP"
-form='JSON'
-limit=15
-
-res=requests.get(f'{base}{dataid}?Authorization={auth}&stationId={stationid}&elementName={elem}&format={form}&limit={limit}')
-
-print(res.status_code)
-d=json.loads(res.content.decode('utf-8'))
-
-with open('weather.json','w') as f:
-    json.dump(d,f)
-
-temp=d['cwbopendata']['location'][0]['weatherElement'][3]['elementValue']
-loca=d['cwbopendata']['location'][0]['locationName']
-tp=json.dumps(temp,indent=4)
-lo=json.dumps(loca,indent=4)
-print(tp.encode('utf-8').decode('unicode_escape'))
-print(lo.encode('utf-8').decode('unicode_escape'))
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
@@ -71,3 +45,5 @@ def main():
         for item in items:
             print(u'{0} ({1})'.format(item['name'], item['id']))
 
+if __name__ == '__main__':
+    main()
